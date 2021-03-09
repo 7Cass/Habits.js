40d3d5077498a3f99fd8ad91b26b1811b1c0485a
João Pedro Arruda <jaaopbr@gmail.com>
2021-03-09 17:23:28 -0300

    Feat/Fix: Added login path and page, Fixed import in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
+import Login from "../pages/Login";
 
-export const Routes = () => {
+const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
         <HomePage />
+      </Route>
+      <Route>
+        <Login exact path="/login" />
       </Route>
     </Switch>
   );
