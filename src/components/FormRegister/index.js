8058f36baa9bf4e7d67ba8208ed052b3d4471c04
Alex Schmitt <alex.schmitt@outlook.com>
2021-03-09 18:19:54 -0300

    feat: :sparkles: Criação do componente FormRegister
    Criação do FormRegister e funcionalidades de validação de campos
    
    Faltam estilização e chamar o endpoint para criar o usuário

--- src/components/FormRegister/index.js
+++ src/components/FormRegister/index.js
@@ -1,5 +1,78 @@
+// material ui
+import { TextField, Typography } from "@material-ui/core";
+
+// react router dom
+import { useHistory, Link } from "react-router-dom";
+
+// react hook form + yup + resolvers
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+//--------------------------------------------
+const errorRequired = "Campo obrigatório";
+const schema = yup.object().shape({
+  username: yup.string().required(errorRequired),
+  password: yup
+    .string()
+    .min(6, "Senha deve ter no mínimo 6 caracteres")
+    .required(errorRequired),
+  email: yup
+    .string()
+    .email("Formato requerido: ***@***")
+    .required(errorRequired),
+});
+
+//--------------------------------------------
 const FormRegister = () => {
-  return <div>Form Register</div>;
+  const history = useHistory();
+  const { register, handleSubmit, errors, reset } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onRegister = (data) => {
+    console.log(data);
+    reset();
+    history.push("/login");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onRegister)}>
+        <TextField
+          name="username"
+          label="Username"
+          variant="outlined"
+          size="small"
+          inputRef={register}
+          error={!!errors.username}
+          helperText={errors.username?.message}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          variant="outlined"
+          size="small"
+          type="password"
+          inputRef={register}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+        />
+        <TextField
+          name="email"
+          label="E-mail"
+          variant="outlined"
+          size="small"
+          type="email"
+          inputRef={register}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+        />
+        <button type="submit">Enviar</button>
+        <Typography>Já registrado?</Typography>
+        <Link to="/login">login</Link>
+      </form>
+    </div>
+  );
 };
 
 export default FormRegister;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,16 @@
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
-
+import Register from "../pages/Register";
+//-----------------------------------------------
 export const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
         <HomePage />
+      </Route>
+      <Route path="/register">
+        <Register />
       </Route>
     </Switch>
   );
