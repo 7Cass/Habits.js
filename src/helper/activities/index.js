d53cd31c21149d9711d2e35644492a479ee9287a
Alex Schmitt <alex.schmitt@outlook.com>
2021-03-11 12:49:42 -0300

    feat: FormUpdateHabit e funções de endpoints
    FormUpdateHabit e funções com endpoints

---
+++ src/helper/activities/index.js
@@ -0,0 +1,5 @@
+export const getGroupActivities = () => `/activities/`;
+export const getOneActivity = (activityID) => `/activities/${activityID}/`;
+export const postCreateActivity = () => `/activities/`;
+export const patchUpdateActivity = (activityID) => `/activities/${activityID}/`;
+export const deleteActivity = (activityID) => `/activities/${activityID}/`;
---
+++ src/helper/users/index.js
@@ -0,0 +1,5 @@
+export const getUsers = () => `/users/`;
+export const getOneUser = (userID) => `/users/${userID}/`;
+export const postLogin = () => `/sessions/`;
+export const postCreateUser = () => `/users/`;
+export const patchUpdateUser = (userID) => `/users/${userID}/`;
