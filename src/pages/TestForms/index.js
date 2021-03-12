401dec34493804fdb18e244bee39fd84d71ed7b0
Diego Silva <dev.diegosilva@hotmail.com>
2021-03-12 09:30:22 -0300

    support: BotÃ£o para ir pro grupo
    botÃ£o para ir para pagina de grupos para nao atualizar a pagina

--- src/pages/TestForms/index.js
+++ src/pages/TestForms/index.js
@@ -2,11 +2,16 @@
 import FormUpdateHabit from "../../components/FormUpdateHabit";
 import CreateHabitForm from "../../components/CreateHabitForm";
 //-------------------------------------
+//-------------------------------------
+import { useHistory } from "react-router-dom";
+//-------------------------------------
 const TestForms = () => {
+  const history = useHistory();
   return (
     <>
       <FormUpdateHabit id={34} />
       <CreateHabitForm />
+      <button onClick={() => history.push("/group")}>Para group</button>
     </>
   );
 };
