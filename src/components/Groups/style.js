be27b862fed0de02815acc62d0e4bd5b452bb96c
Diego Silva <dev.diegosilva@hotmail.com>
2021-03-11 22:59:34 -0300

    feat: Grupos
    Componente que renderiza todos os grupos criados na API, falta paginação

---
+++ src/components/Groups/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const CardGroup = styled.div`
+  display: flex;
+  text-align: center;
+  border: 2px solid black;
+  flex-direction: column;
+  margin: 15px;
+`;
+
+export const GroupsContent = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-around;
+`;
