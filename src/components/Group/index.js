cb8628f97a9e18ab86b618caee739a464c720bfb
Diego Silva <dev.diegosilva@hotmail.com>
2021-03-11 22:58:39 -0300

    feat: Grupo
    criei o componente que recebe as informaçoes do grupo que o usuario
    logado participa;

---
+++ src/components/Group/index.js
@@ -0,0 +1,92 @@
+import API from "../../services";
+import { useEffect, useState } from "react";
+import { GroupContent, CardGroup, userCard, generalCard } from "./style";
+import ModalActivity from "../ModalActivity";
+
+//helpers =>
+
+import { useChecked } from "../../providers/user";
+import { getOneUser } from "../../helper/users";
+import { getOneGroup } from "../../helper/groups";
+import { deleteActivity } from "../../helper/activities";
+
+//==============================================
+const Group = () => {
+  const [group, setGroup] = useState([]);
+  const { userId } = useChecked();
+
+  const [token] = useState(() => {
+    const Token = localStorage.getItem("token") || "";
+
+    if (!Token) {
+      return "";
+    }
+    return JSON.parse(Token);
+  });
+
+  const getGroup = async (data) => {
+    try {
+      const takeUser = await API.get(getOneUser(userId), data);
+      console.log(takeUser.data);
+
+      const takeGroup = await API.get(getOneGroup(takeUser.data.group), data);
+      console.log(takeGroup.data);
+
+      setGroup(takeGroup.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deteleActivity = async (actId, data) => {
+    try {
+      // eslint-disable-next-line
+      const delActivity = await API.delete(deleteActivity(actId), {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getGroup();
+  }, []);
+
+  return (
+    <GroupContent>
+      <CardGroup>
+        <h1>{group.name}</h1>
+        <h4>{group.description}</h4>
+        <generalCard>
+          <h2>Metas: </h2>
+          <button>Nova Meta</button>
+          {group.goals !== undefined
+            ? group.goals.map((goal) => <h3>{goal.title}</h3>)
+            : null}
+        </generalCard>
+        <generalCard>
+          <h2>Atividades</h2>
+          <ModalActivity group={group} setGroup={setGroup} />
+          {group.activities !== undefined
+            ? group.activities.map((activity) => (
+                <div>
+                  <h3>{activity.title}</h3>
+                  <button onClick={() => deteleActivity(activity.id)}>
+                    Delete
+                  </button>
+                </div>
+              ))
+            : null}
+        </generalCard>
+        <userCard>
+          <h2>Usuarios:</h2>
+          {group.users !== undefined
+            ? group.users.map((user) => <h3>{user.username}</h3>)
+            : null}
+        </userCard>
+      </CardGroup>
+    </GroupContent>
+  );
+};
+
+export default Group;
