fdeb67b726f75b9ee9668869e9df62c2c407c835
Alex Schmitt <alex.schmitt@outlook.com>
2021-03-15 16:36:27 -0300

    style: :fire: removendo páginas desnecessárias

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,9 +25,6 @@ const Routes = () => {
       <Route exact path="/login">
         <Login />
       </Route>
-      <Route exact path="/new-group">
-        <NewGroup />
-      </Route>
       <Route exact path="/groups">
         <GroupsPage />
       </Route>
