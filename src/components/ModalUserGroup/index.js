cdbd59dcf87be52d9d79bc9e5233504fdc58ccc9
Diego Silva <dev.diegosilva@hotmail.com>
2021-03-16 18:06:41 -0300

    feat: :sparkles: Modal grupo do usuario
    Passei o grupo do usuario para o modal

---
+++ src/components/ModalUserGroup/index.js
@@ -0,0 +1,45 @@
+import Modal from "@material-ui/core/Modal";
+import { useState } from "react";
+import Group from "../Group";
+import { useModalUserGroup } from "../../styles/makeStyles";
+import getModalStyle from "../../styles/modalStyles";
+import Button from "../Button";
+
+//--------------------------------------------------
+const ModalUserGroup = () => {
+  const classes = useModalUserGroup();
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paperStyles1}>
+      <Group handleClose={handleClose} />
+    </div>
+  );
+
+  return (
+    <div>
+      <Button size="small" styled="filled" onClick={() => handleOpen()}>
+        <i class="fas fa-info-circle"></i>
+        Detalhes
+      </Button>
+      <Modal
+        open={open}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalUserGroup;
